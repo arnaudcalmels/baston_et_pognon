7dd7d9bc93e9756c2deda80fa09057c2bf1ec5db
ArnaudCalmels <calmelsarnaud@gmail.com>
2021-04-12 19:09:32 +0200

    jwt configuration - service to getCurrentUser

---
+++ back/src/Service/UserService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class UserService
+{
+
+    /** @var  TokenStorageInterface */
+    private $tokenStorage;
+
+    /**
+     * @param TokenStorageInterface  $storage
+     */
+    public function __construct(TokenStorageInterface $storage)
+    {
+        $this->tokenStorage = $storage;
+    }
+
+    public function getCurrentUser()
+    {
+        $token = $this->tokenStorage->getToken();
+        if ($token instanceof TokenInterface) {
+
+            /** @var User $user */
+            $user = $token->getUser();
+            return $user;
+
+        } else {
+            return null;
+        }
+    }
+}

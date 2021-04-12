15c771dbb9826f0453b7efe89014a3caf107a008
stephanie-calmels <stephcalmels82@gmail.com>
2021-04-12 17:33:31 +0200

    button component

---
+++ front/src/components/Button/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = ({ color, children, onClick}) => {
+    return (
+        <button 
+            onClick={onClick} 
+            style={{
+                backgroundColor: color,
+                border: 'none',
+                padding: '0.5rem 1rem',
+                borderRadius: '25px',
+                padding: '1rem 2rem',
+                margin: '0.5rem'
+                }}>
+            {children}
+        </button>
+    );
+};
+
+Button.propTypes = {
+    color: PropTypes.string,
+    children: PropTypes.string,
+    onClick: PropTypes.func
+};
+
+export default Button;
--- front/src/components/Navigation/index.js
+++ front/src/components/Navigation/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Button from '../Button';
 // import PropTypes from 'prop-types';
 
 const Navigation = (props) => {
@@ -10,6 +11,8 @@ const Navigation = (props) => {
             <Link to="/scenario">Scénarios</Link>   
             <Link to="/jeu">Parties en ligne</Link>   
             <Link to="/actualité">Actualités</Link>   
+            <Button color='#eee' children='Inscription'></Button>
+            <Button color='#eee' children='Connexion'></Button>
         </div>
     );
 };

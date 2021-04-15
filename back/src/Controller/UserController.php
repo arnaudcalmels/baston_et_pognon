c4ce036b563d356fa240b23d08f792a7b5c2325f
ArnaudCalmels <calmelsarnaud@gmail.com>
2021-04-15 16:04:49 +0200

    add: JWT config, Edit & Delete User, remove useless UserService

--- back/src/Controller/UserController.php
+++ back/src/Controller/UserController.php
@@ -3,22 +3,20 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Service\UserService;
 use App\Repository\UserRepository;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Validator\ConstraintViolation;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 
 class UserController extends AbstractController
 {
-    public function new(Request $request, UserPasswordEncoderInterface $passwordEncoder, ValidatorInterface $validator): Response
+    public function new(Request $request, UserPasswordEncoderInterface $passwordEncoder, ValidatorInterface $validator): JsonResponse
     {
         $requestContent = json_decode($request->getContent());
 
@@ -51,9 +49,9 @@ class UserController extends AbstractController
         return new JsonResponse($data, $statusCode);
     }
 
-    public function profile(UserService $userService): Response
+    public function profile(): JsonResponse
     {
-        $currentUser = $userService->getCurrentUser();
+        $currentUser = $this->getUser();
 
         if ($currentUser === null) {
             return new JsonResponse('Utilisateur non identifié', 200);
@@ -62,6 +60,75 @@ class UserController extends AbstractController
         $data = $this->normalizeUser($currentUser);
 
         return new JsonResponse($data, 200);
+    }
+
+    public function edit(
+        Request $request,
+        User $user,
+        UserPasswordEncoderInterface $passwordEncoder,
+        ValidatorInterface $validator,
+        JWTTokenManagerInterface $JWTManager): JsonResponse
+    {
+        $currentUser = $this->getUser();
+
+        if ($user !== $currentUser) {
+            $data = 'Vous n\'êtes pas autorisé à modifier cet utilisateur.';
+            $statusCode = 403;
+
+        } else {
+            $requestContent = json_decode($request->getContent());
+            if (isset($requestContent->email)) {
+                $user->setEmail($requestContent->email);
+                $emailChanged = true;
+            }
+            if (isset($requestContent->password)) {
+                if ($requestContent->password === $requestContent->confirmPassword) {
+                    $user->setPassword($passwordEncoder->encodePassword($user, $requestContent->password));
+                }
+            }
+            if (isset($requestContent->pseudo)) {
+                $user->setPseudo($requestContent->pseudo);
+            }
+            if (isset($requestContent->avatar)) {
+                $user->setAvatar($requestContent->avatar);
+            }
+
+            $errors = $validator->validate($user);
+
+            if (count($errors) > 0) {
+                $data = [];
+                foreach ($errors as $error) {
+                    /* @var ConstraintViolation $error */
+                    $data[] = $error->getMessage();
+                    $statusCode = 403;
+                }
+            } else {
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                $statusCode = 200;
+                $data = $this->normalizeUser($user);
+                if (isset($emailChanged) && $emailChanged) {
+                    $data['token'] = $JWTManager->create($user);
+                }
+            }
+        }
+
+        return new JsonResponse($data, $statusCode);
+    }
+
+    public function delete(Request $request, User $user): JsonResponse
+    {
+        $currentUser = $this->getUser();
+
+        if ($user === $currentUser); {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($user);
+            $entityManager->flush();   
+        }
+
+        return new JsonResponse('Suppression OK', 200);
     }
 
     /* 

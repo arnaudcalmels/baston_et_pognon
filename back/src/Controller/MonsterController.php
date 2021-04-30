c2420e2dc4481b268b9ee9c794748ad2b9934c11
ArnaudCalmels <calmelsarnaud@gmail.com>
2021-04-30 09:56:18 +0200

    add: route to get monster, adaptative route for create monster in differents contexts

--- back/src/Controller/MonsterController.php
+++ back/src/Controller/MonsterController.php
@@ -5,13 +5,14 @@ namespace App\Controller;
 use App\Entity\Action;
 use App\Entity\Monster;
 use App\Entity\Caracteristics;
-use App\Repository\PlaceRepository;
+use App\Entity\Place;
+use App\Entity\Scenario;
+use App\Entity\WanderingMonsterGroup;
+use Doctrine\ORM\EntityManager;
 use App\Validator\MonsterValidator;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Validator\ConstraintViolationList;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
@@ -19,60 +20,67 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MonsterController extends AbstractController
 {
-    public function newMonsterForAPlace(Request $request, PlaceRepository $placeRepo, ValidatorInterface $validator)
+    /**
+     * Get a monster
+     *
+     * @param Monster $monster
+     * @return JsonResponse
+     */
+    public function monster(Monster $monster = null, int $id): JsonResponse
     {
-        $currentUser = $this->getUser();
+        if (!$monster) {
+            $data = ['Le monstre d\'id ('.$id.') n\'existe pas'];
+            $statusCode = 404;
+        } else {
+            $data = $this->normalizeMonster($monster);
+            $statusCode = 200;
+        }
 
+        return new JsonResponse($data, $statusCode);
+    }
+
+    /**
+    * Create a monster in object context
+    *
+    * @param Request $request
+    * @param string $slug
+    * @param ValidatorInterface $validator
+    * @return JsonResponse|void
+    */
+    public function new(Request $request, string $slug, ValidatorInterface $validator)
+    {
         $monsterValidator = new MonsterValidator($validator);
-        /**
-         * @var ConstraintViolationList $errors
-         */
-        $errors = $monsterValidator->validateRequestDatas($request->getContent());
+
+        $errors = $monsterValidator->validateRequestDatas($request->getContent(), true, $slug);
 
         if (count($errors) > 0) {
             $data = $errors;
             $statusCode = 403;
         } else {
-            $requestContent = json_decode($request->getContent());
             $em = $this->getDoctrine()->getManager();
 
-            $place = $placeRepo->find($requestContent->placeId);
-            if ($place->getScenario()->getOwner() !== $currentUser) {
-                $data = ['Vous n\'êtes pas autorisé à modifier ce scénario'];
+            $requestContent = json_decode($request->getContent());
+
+            switch($slug) {
+                case 'place':
+                    $monster = $this->createMonsterForAPlace($requestContent, $em);
+                    break;
+
+                case 'scenario':
+                    $monster = $this->createMonsterForAScenario($requestContent, $em);
+                    break;
+
+                case 'wanderGroup':
+                    $monster = $this->createMonsterForAWanderingMonsterGroup($requestContent, $em);
+                    break;
+            }
+
+            if (!$monster) {
+                // Le current user n'est pas le owner
+                $data = ['Vous n\'êtes pas autorisé à effectuer cette action'];
                 $statusCode = 403;
+
             } else {
-                $caracteristics = $requestContent->caracteristics;
-
-                $caracteristicsObject = new Caracteristics();
-
-                $caracteristicsObject->setArmor($caracteristics->armor);
-                $caracteristicsObject->setLifePoints($caracteristics->lifePoints);
-
-                $em->persist($caracteristicsObject);
-
-                foreach ($caracteristics->actions as $action) {
-                    $actionObject = new Action();
-                    $actionObject->setDamages($action->damages);
-                    $actionObject->setDistance($action->distance);
-                    $actionObject->setFrequency($action->frequency);
-                    $actionObject->setHeal($action->heal);
-                    $actionObject->setIsSpecial($action->isSpecial);
-
-                    $em->persist($actionObject);
-
-                    $caracteristicsObject->addAction($actionObject);
-                }
-
-
-                
-                $monster = new Monster();
-                $monster->setName($requestContent->name);
-                $monster->setIsBoss($requestContent->isBoss);
-                $monster->setHasBooster($requestContent->hasBooster);
-                $monster->setLevel($requestContent->level);
-                $monster->setPicture($requestContent->picture);
-                $monster->setCaracteristics($caracteristicsObject);
-                $monster->setPlace($place);
                 $em->persist($monster);
 
                 $errorsObject = $validator->validate($monster);
@@ -83,16 +91,154 @@ class MonsterController extends AbstractController
                 } else {
                     $em->flush();
 
-                    $data = $this->normalizeMonster($monster);
-                    $statusCode = 201;
+                    $place = $monster->getPlace();
+                    if ($place) {
+                        $scenarioId = $place->getScenario()->getId();
+                    } else {
+                        $scenarioId = $monster->getWanderingMonsterGroup()->getScenario()->getId();
+                    }
+
+                    return $this->redirectToRoute('api_scenario', ['id' => $scenarioId]);
                 }
-            }
+            }               
         }
 
         return new JsonResponse($data, $statusCode);
     }
 
-    public function normalizeMonster($monster): array
+    /**
+    * Create a monster object in Place context
+    *
+    * @param \StdClass $object
+    * @param EntityManager $em
+    * @return Monster|null
+    */
+    private function createMonsterForAPlace(\StdClass $object, EntityManager $em): ?Monster
+    {
+        $currentUser = $this->getUser();
+
+        $place = $em->getRepository(Place::class)->find($object->placeId);
+
+        if ($place->getScenario()->getOwner() !== $currentUser) {
+
+            return null;
+        } else {
+
+            $monster = $this->monsterCreation($object, $em);
+
+            $place->addMonster($monster);
+
+            return $monster;
+        }
+    }
+
+    /**
+     * Create a monster object in Scenario context
+     *
+     * @param \StdClass $object
+     * @param EntityManager $em
+     * @return Monster|null
+     */
+    private function createMonsterForAScenario(\StdClass $object, EntityManager $em): ?Monster
+    {
+        $currentUser = $this->getUser();
+
+        $scenario = $em->getRepository(Scenario::class)->find($object->scenarioId);
+
+        if ($scenario->getOwner() !== $currentUser) {
+
+            return null;
+        } else {
+
+            $wanderingGroup = new WanderingMonsterGroup();
+
+            $monster = $this->monsterCreation($object, $em);
+            
+            $wanderingGroup->addMonster($monster);
+            $scenario->addWanderingMonster($wanderingGroup);
+
+            return $monster;
+        }
+    }
+
+    /**
+     * Create a monster object in WanderingMonsterGroup context
+     *
+     * @param \StdClass $object
+     * @param EntityManager $em
+     * @return Monster|null
+     */
+    private function createMonsterForAWanderingMonsterGroup(\StdClass $object, EntityManager $em): ?Monster
+    {
+        $currentUser = $this->getUser();
+
+        $wanderingGroup = $em->getRepository(WanderingMonsterGroup::class)->find($object->wanderGroupId);
+
+        if ($wanderingGroup->getScenario()->getOwner() !== $currentUser) {
+
+            return null;
+        } else {
+
+            $monster = $this->monsterCreation($object, $em);
+
+            $wanderingGroup->addMonster($monster);
+
+            return $monster;
+        }
+    }
+
+    /**
+     * Create a monster
+     *
+     * @param \StdClass $object
+     * @param EntityManager $em
+     * @return Monster
+     */
+    private function monsterCreation(\StdClass$object, EntityManager $em): Monster
+    {
+        // Création de l'objet caracteristics
+        $caracteristics = $object->caracteristics;
+
+        $caracteristicsObject = new Caracteristics();
+
+        $caracteristicsObject->setArmor($caracteristics->armor);
+        $caracteristicsObject->setLifePoints($caracteristics->lifePoints);
+
+        $em->persist($caracteristicsObject);
+
+        // Création et ajout des actions à l'abjet caracteristics
+        foreach ($caracteristics->actions as $action) {
+            $actionObject = new Action();
+            $actionObject->setDamages($action->damages);
+            $actionObject->setDistance($action->distance);
+            $actionObject->setFrequency($action->frequency);
+            $actionObject->setHeal($action->heal);
+            $actionObject->setIsSpecial($action->isSpecial);
+
+            $em->persist($actionObject);
+
+            $caracteristicsObject->addAction($actionObject);
+        }
+
+        // Création du monstre, assignation de ses caractéristiques
+        $monster = new Monster();
+        $monster->setName($object->name);
+        $monster->setIsBoss($object->isBoss);
+        $monster->setHasBooster($object->hasBooster);
+        $monster->setLevel($object->level);
+        $monster->setPicture($object->picture);
+        $monster->setCaracteristics($caracteristicsObject);
+
+        return $monster;
+    }
+
+    /**
+     * Normalize monster object
+     *
+     * @param Monster|Monster[] $monster
+     * @return array
+     */
+    private function normalizeMonster($monster): array
     {
         $normalizers = [new ObjectNormalizer()];
         $serializer = new Serializer($normalizers);
--- back/src/Validator/MonsterValidator.php
+++ back/src/Validator/MonsterValidator.php
@@ -2,7 +2,10 @@
 
 namespace App\Validator;
 
+use App\Entity\Monster;
 use App\Entity\Place;
+use App\Entity\Scenario;
+use App\Entity\WanderingMonsterGroup;
 use App\Validator\ContainsIdOfEntityClass;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Validator\ConstraintViolation;
@@ -22,84 +25,124 @@ class MonsterValidator
         $this->serializer = new Serializer([new ObjectNormalizer()], [new JsonEncoder()]);
     }
 
-    public function validateRequestDatas(string $requestContent)
+    public function validateRequestDatas(string $requestContent, $isNew = false, string $parentEntityName = null)
     {
         $datas = $this->serializer->decode($requestContent, 'json');
 
+        if ($isNew) {
+            // S'il s'agit d'une création on vérifie que l'entité parent est définie et existe
+            $availableParentEntity = [
+                'scenario' => Scenario::class,
+                'place' => Place::class,
+                'wanderGroup' => WanderingMonsterGroup::class,
+            ];
+    
+            $className = $availableParentEntity[$parentEntityName];
+
+            $parentConstraints = new Assert\Collection([
+                'allowExtraFields' => true,
+                'fields' => [
+                    $parentEntityName.'Id' => [
+                        new Assert\NotBlank(),
+                        new Assert\Positive(),
+                        new ContainsIdOfEntityClass($className),
+                    ],    
+                ],
+            ]);
+
+            $parentValidationErrors = $this->validator->validate($datas, $parentConstraints);
+        } else {
+            // Sinon on vérifie que le monstre est défini et existe
+            $parentConstraints = new Assert\Collection([
+                'allowExtraFields' => true,
+                'fields' => [
+                    'monsterId' => [
+                        new Assert\NotBlank(),
+                        new Assert\Positive(),
+                        new ContainsIdOfEntityClass(Monster::class),
+                    ],    
+                ],
+            ]);
+        }
+
+
         $constraints = new Assert\Collection([
-            'placeId' => [
-                new Assert\NotBlank(),
-                new Assert\Positive(),
-                new ContainsIdOfEntityClass(Place::class),
-            ],
-            'name' => [
-                new Assert\NotBlank(),
-            ],
-            'isBoss' => [
-                new Assert\Type('bool'),
-                new Assert\NotNull(),
-            ],
-            'hasBooster' => [
-                new Assert\Type('bool'),
-                new Assert\NotNull(),
-            ],
-            'level' => [
-                new Assert\Positive(),
-                new Assert\NotNull(),
-            ],
-            'picture' => [
-                new Assert\Type('string'),
-            ],
-            'caracteristics' => new Assert\Required([
-                new Assert\Collection([
-                    'armor' => [
-                        new Assert\Positive(),
-                        new Assert\NotNull(),
-                    ],
-                    'lifePoints' => [
-                        new Assert\Positive(),
-                        new Assert\NotNull(),
-                    ],
-                    'actions' => new Assert\Required([
-                        new Assert\Type('array'),
-                        new Assert\Count(['min' => 1]),
-                        new Assert\All([
-                            new Assert\Collection([
-                                'damages' => [
-                                    new Assert\Positive(),
-                                    new Assert\NotNull(),
-                                ],
-                                'distance' => [
-                                    new Assert\Type('bool'),
-                                    new Assert\NotNull(),
-                                ],
-                                'frequency' => [
-                                    new Assert\Positive(),
-                                    new Assert\NotNull(),
-                                ],
-                                'heal' => [
-                                    new Assert\Type('bool'),
-                                    new Assert\NotNull(),
-                                ],
-                                'isSpecial' => [
-                                    new Assert\Type('bool'),
-                                    new Assert\NotNull(),
-                                ],
+            'allowExtraFields' => true,
+            'fields' => [
+                'name' => [
+                    new Assert\NotBlank(),
+                ],
+                'isBoss' => [
+                    new Assert\Type('bool'),
+                    new Assert\NotNull(),
+                ],
+                'hasBooster' => [
+                    new Assert\Type('bool'),
+                    new Assert\NotNull(),
+                ],
+                'level' => [
+                    new Assert\Positive(),
+                    new Assert\NotNull(),
+                ],
+                'picture' => [
+                    new Assert\Type('string'),
+                ],
+                'caracteristics' => new Assert\Required([
+                    new Assert\Collection([
+                        'armor' => [
+                            new Assert\Positive(),
+                            new Assert\NotNull(),
+                        ],
+                        'lifePoints' => [
+                            new Assert\Positive(),
+                            new Assert\NotNull(),
+                        ],
+                        'actions' => new Assert\Required([
+                            new Assert\Type('array'),
+                            new Assert\Count(['min' => 1]),
+                            new Assert\All([
+                                new Assert\Collection([
+                                    'damages' => [
+                                        new Assert\Positive(),
+                                        new Assert\NotNull(),
+                                    ],
+                                    'distance' => [
+                                        new Assert\Type('bool'),
+                                        new Assert\NotNull(),
+                                    ],
+                                    'frequency' => [
+                                        new Assert\Positive(),
+                                        new Assert\NotNull(),
+                                    ],
+                                    'heal' => [
+                                        new Assert\Type('bool'),
+                                        new Assert\NotNull(),
+                                    ],
+                                    'isSpecial' => [
+                                        new Assert\Type('bool'),
+                                        new Assert\NotNull(),
+                                    ],
+                                ]),
                             ]),
                         ]),
                     ]),
                 ]),
-            ]),
+    
+            ],
         ]);
 
-        // return [];
         $validationErrors = $this->validator->validate($datas, $constraints);
-        // $errorsString = (string) $validationErrors;
-        // dd($errorsString);
+
         $errors = [];
+
+        if ($parentValidationErrors) {
+            /** @var ConstraintViolation $error */
+            foreach ($parentValidationErrors as $error) {
+                $errors[$error->getPropertyPath()] = $error->getMessage();
+            }
+        }
         /** @var ConstraintViolation $error */
         foreach ($validationErrors as $error) {
-            // dd($error->getRoot());
             $errors[$error->getPropertyPath()] = $error->getMessage();
         }
 

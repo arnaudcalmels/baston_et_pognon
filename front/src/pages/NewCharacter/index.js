822f23cce8b32c137da4552261ac66cf709ba3b9
stephanie-calmels <stephcalmels82@gmail.com>
2021-09-15 16:08:35 +0200

    add: styles for ne character

--- front/src/pages/NewCharacter/index.js
+++ front/src/pages/NewCharacter/index.js
@@ -4,6 +4,7 @@ import { useHistory } from 'react-router-dom';
 import FileBase64 from 'react-file-base64';
 
 import Button from '../../components/Button';
+import Title from '../../components/Title';
 
 import PropTypes from 'prop-types';
 
@@ -38,31 +39,29 @@ const NewCharacter = ({ newCharacter, getProfessions, getRaces, professions, rac
   return (
     <div className={styles['main']}>
 
-      <h2 className={styles['name']}>{'Créer un nouveau personnage'}</h2>
+      <Title title={'Créer un nouveau personnage'} id={styles['title']}/>
 
-      <div id={styles['block_form']} className={styles['show_form']}>
-        <Formik
-          initialValues={{
-          name: '',
-          sex: '',
-          professionId: '',
-          raceId: '',
-          picture: null
-          }}
-          validate={values => {
-            const errors = {};
-            if (!values.name || !values.sex || !values.professionId || !values.raceId) {
-              errors.all = 'Veuillez remplir tous les champs !';
-            }
-            return errors;
-          }}
-          onSubmit={(values) => handleSubmit(values)}
-        >
-        {
-          (props) => (
-            <Form className={styles['form']}>
-              {/* <ErrorMessage name='all' component='div' className={styles['error_message']}/> */}
-
+      <Formik
+        initialValues={{
+        name: '',
+        sex: '',
+        professionId: '',
+        raceId: '',
+        picture: null
+        }}
+        validate={values => {
+          const errors = {};
+          if (!values.name || !values.sex || !values.professionId || !values.raceId) {
+            errors.all = 'Veuillez remplir tous les champs !';
+          }
+          return errors;
+        }}
+        onSubmit={(values) => handleSubmit(values)}
+      >
+      {
+        (props) => (
+          <Form className={styles['form']}>
+            <div className={styles['character_name']}>
               <label htmlFor="name" className={styles['form_label']}>Nom :</label>
               <Field
                 className={styles['form_field']}
@@ -71,18 +70,23 @@ const NewCharacter = ({ newCharacter, getProfessions, getRaces, professions, rac
                 type="text"
               />
 
-              <div className={styles['form_label']}>Sexe :</div>
-              <div role="group" className={styles['form_group']}>
-                <label className={styles['form_radio']}>
-                  <Field type="radio" name="sex" value="M" className={styles['form_radio-input']}/>
-                  M
-                </label>
-                <label className={styles['form_radio']}>
-                  <Field type="radio" name="sex" value="F" className={styles['form_radio-input']}/>
-                  F
-                </label>
-              </div>
+            </div>
 
+            {/* eslint-disable-next-line */}
+            <img id={styles['image_preview']} src={picture?.base64} alt={picture?.name}/>
+
+            <div className={styles['character_picture']}>
+              <label htmlFor="picture" className={styles['form_label']}>Ajouter une image  :</label>
+              <FileBase64
+                multiple={false}
+                onDone={getFile.bind(this, props)}
+              />
+            </div>
+
+            {/* eslint-disable-next-line */}
+            <img id={styles['image_preview']} src={picture?.base64} alt={picture?.name}/>
+
+            <div className={styles['caracteristics']}>
               <label htmlFor="professionId" className={styles['form_label']}>Classe :</label>
               <Field
                 className={styles['form_field']}
@@ -97,8 +101,7 @@ const NewCharacter = ({ newCharacter, getProfessions, getRaces, professions, rac
                       <option value={profession.id}>{profession.name}</option>
                     ))
                   }
-                </Field>
-
+              </Field>
 
               <label htmlFor="raceId" className={styles['form_label']}>Race :</label>
               <Field
@@ -116,40 +119,40 @@ const NewCharacter = ({ newCharacter, getProfessions, getRaces, professions, rac
                   }
               </Field>
 
+              <div className={styles['form_label']}>Sexe :</div>
+              <div role="group" className={styles['form_group']}>
+                <label className={styles['form_radio']}>
+                  <Field type="radio" name="sex" value="M" className={styles['form_radio-input']}/>
+                  M
+                </label>
+                <label className={styles['form_radio']}>
+                  <Field type="radio" name="sex" value="F" className={styles['form_radio-input']}/>
+                  F
+                </label>
+              </div>
+            </div>  
+
             <span className={styles['info']}>Tous les champs sont obligatoires.</span>
 
-            <div className={`${styles['newCharacter_picture']} ${styles['form_item']}`}>
-                <label htmlFor="picture" className={styles['form_label']}>Image :</label>
-                <FileBase64
-                  multiple={false}
-                  onDone={getFile.bind(this, props)}
-                />
-              </div>
+            <Button
+              id={styles['submit_button']}
+              type="submit"
+              color='#eee'
+              children='Valider'
+            />
+        
+          </Form>
 
-              {/* eslint-disable-next-line */}
-              <img id={styles['image_preview']} src={picture?.base64} alt={picture?.name}/>
+        )
+      }
+      </Formik>
 
-
-              <Button
-                id={styles['submit_button']}
-                type="submit"
-                color='#eee'
-                children='Valider'
-              />
-          
-            </Form>
-
-          )
-        }
-        </Formik>
-
-        <Button
-          id={styles['cancel_button']}
-          color='#eee'
-          children='Annuler'
-          onClick={() => backToList()}
-        />
-      </div>
+      <Button
+        id={styles['cancel_button']}
+        color='#eee'
+        children='Annuler'
+        onClick={() => backToList()}
+      />
 
       <div className={styles['profession_infos']}>
         Caractéristiques détaillées des classes
